0b0d971a92c23db2fc7ab398f5a646b669f43a77
pmchavezc <pablomchavez620@gmail.com>
2024-04-03 22:34:37 -0600

    prueba

---
+++ Aeropuerto/src/test/java/com/example/Aeropuerto/AeropuertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aeropuerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeropuertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
